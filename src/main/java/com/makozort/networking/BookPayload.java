16e59cd4bd4ece5feba21fcafd334474d2044cde
Jack M <67140936+makozort@users.noreply.github.com>
2024-07-24 03:51:28 +1000

    first commit

--- src/main/java/com/makozort/networking/BookPayload.java
+++ src/main/java/com/makozort/networking/BookPayload.java
@@ -3,7 +3,8 @@ package com.makozort.networking;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.network.codec.PacketCodec;
 import net.minecraft.network.codec.PacketCodecs;
-import net.minecraft.network.packet.CustomPayload;
+
+import net.minecraft.network.packet.payload.CustomPayload;
 
 public record BookPayload(String str) implements CustomPayload {
     public static final CustomPayload.Id<BookPayload> ID = new CustomPayload.Id<>(NetworkingConstants.HIGHLIGHT_PACKET_ID);

2be866272706ff76074522f293b96a0b5f9e25ad
Jack M <67140936+makozort@users.noreply.github.com>
2024-07-24 03:16:45 +1000

    first commit

---
+++ src/client/java/com/makozort/screens/HomeScreen.java
@@ -0,0 +1,55 @@
+package com.makozort.screens;
+
+import com.makozort.PDA;
+import io.wispforest.owo.ui.base.BaseOwoScreen;
+
+import io.wispforest.owo.ui.component.Components;
+import io.wispforest.owo.ui.container.Containers;
+import io.wispforest.owo.ui.container.FlowLayout;
+import io.wispforest.owo.ui.container.StackLayout;
+import io.wispforest.owo.ui.core.*;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.NotNull;
+
+public class HomeScreen extends BaseOwoScreen<FlowLayout> {
+
+    public static final int BOOK_HEIGHT =  200;
+    public static final int BOOK_WIDTH = 300;
+    public String[] strings = {"test", "best", "fest", "rest"};
+
+
+    public static final Identifier BOOK_TEXTURE = Identifier.of(PDA.MOD_ID, "textures/gui/book.png");
+
+    @Override
+    protected @NotNull OwoUIAdapter<FlowLayout> createAdapter() {
+        return OwoUIAdapter.create(this, Containers::verticalFlow);
+    }
+
+    @Override
+    protected void build(FlowLayout rootComponent) {
+
+
+        StackLayout layout =  Containers.stack(Sizing.content(),Sizing.content())
+            .child(Components.texture(BOOK_TEXTURE,0,0,BOOK_WIDTH,BOOK_HEIGHT,BOOK_WIDTH,BOOK_HEIGHT)
+             );
+
+        int y = 25;
+        for(String i: strings) {
+            layout.child(Components.button(Text.of(i),buttonComponent -> {System.out.println(i); })
+                    .sizing(Sizing.fixed(60),Sizing.content())
+                .positioning(Positioning.relative(110,y)));
+            y +=15;
+        }
+
+        rootComponent
+                .verticalAlignment(VerticalAlignment.CENTER)
+                .horizontalAlignment(HorizontalAlignment.CENTER);
+
+        rootComponent.child(
+            layout
+                .allowOverflow(true)
+                .padding(Insets.right(30))
+        );
+    }
+}
--- src/main/java/com/makozort/PDA.java
+++ src/main/java/com/makozort/PDA.java
@@ -1,35 +1,47 @@
 package com.makozort;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.makozort.networking.BookPayload;
 import com.makozort.reg.ModItems;
 import net.fabricmc.api.ModInitializer;
-
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
-import net.minecraft.network.PacketByteBuf;
-import net.minecraft.server.network.ServerPlayerEntity;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
+import net.minecraft.resource.Resource;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.resource.ResourceType;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class PDA implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final String MOD_ID = "pda";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    // This logger is used to write text to the console and the log file.
+    // It is considered best practice to use your mod id as the logger's name.
+    // That way, it's clear which mod wrote info, warnings, and errors.
+    public static final String MOD_ID = "pda";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    private static final Map<String, String> logmap = new HashMap<>();
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-		ModItems.initialize();
+    @Override
+    public void onInitialize() {
 
 
-		// In your common initializer method
-		PayloadTypeRegistry.playS2C().register(BookPayload.ID, BookPayload.CODEC);
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+        ModItems.initialize();
 
 
+        // In your common initializer method
+        PayloadTypeRegistry.playS2C().register(BookPayload.ID, BookPayload.CODEC);
 
-	}
+
+    }
 }
